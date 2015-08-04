42c812f733c16b30f15e0f6c5a98e422f55dedc7
awsserver <awsserver@outlook.com>
2015-08-04 19:27:45 +0530

    elif

--- python/keywords/elif1.py
+++ python/keywords/elif1.py
@@ -1,4 +1,19 @@
 #__author__ = 'kadmin'
+var = int(raw_input())
+
+if var == 200:
+	print "1- got correct Expressiom"
+	print var
+elif var == 150:
+	print "2- got correct Expression"
+	print var
+elif var == 100:
+	print "3- got correct Expression"
+else:
+	print "4- got correct Expression"
+	print var
+
+print "Good job"
 
 
 
